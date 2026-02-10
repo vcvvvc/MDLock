5ad33fb59b913d0eaf5c64b967dab8b57e2150a1
VW <vcvckw@gmail.com>
2026-02-11 00:23:09 +0800

    chore: sync workspace changes

--- mdlock-dec.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-// runMdlockDec is the entry stub for mdlock-dec behavior.
-func runMdlockDec() error {
-	fmt.Println("mdlock-dec: TODO")
-	return nil
-}
--- mdlock-enc.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-// runMdlockEnc is the entry stub for mdlock-enc behavior.
-func runMdlockEnc() error {
-	fmt.Println("mdlock-enc: TODO")
-	return nil
-}
